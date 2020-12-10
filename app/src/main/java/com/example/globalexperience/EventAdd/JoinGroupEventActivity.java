2fabfb8aa41660852e1dda1fdb74e5130984f793
vleonard <62554949+Vincent-Leonard@users.noreply.github.com>
2020-12-10 12:54:03 +0700

    new activites

---
+++ app/src/main/java/com/example/globalexperience/EventAdd/JoinGroupEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.globalexperience.EventAdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.globalexperience.R;
+
+public class JoinGroupEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_group_event);
+    }
+}
